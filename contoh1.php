f0b1a82fec1fa2d4994f3a8dc2a35a2b4efba481
MhmadArsy <mhmadarsy12@gmail.com>
2021-09-21 09:28:23 +0700

    menambahkan contoh1.php

---
+++ contoh1.php
@@ -0,0 +1,14 @@
+<?php 
+
+$nim = "12201088";
+$nama = "Muhamad Arsy";
+$kelas = "12.3b.13";
+$alamat = "Kp. Sadeng Jalan Kec. Leuwisadeng, Bogor";
+$hobi = "Bermain Badminton dan Sepak Bola";
+
+echo "Nama : $nama<br>";
+echo "NIM : $nim<br>";
+echo "Kelas : $kelas<br>"; 
+echo "Alamat : $alamat<br>";
+echo "Hobi : $hobi<br>";
+?>
